44fc9ac65e9dd9af56633c53ee3ebfefed6e4c22
KimInteger <kjs20151240@gmail.com>
2024-06-10 10:32:44 +0900

    클론코딩 끝

--- public/work-2.js
+++ public/work-2.js
@@ -1,1 +1,21 @@
-console.log('와 work-2.js파일!');
+const createPerson = (callback)=>{
+  let person = {};
+  callback(person, setName);
+};
+
+const setName = (person, callback) => {
+  person.name = '김인티저';
+  callback(person, setAge);
+};
+
+const setAge = (person, callback) =>{
+  person.age = 29;
+  callback(person, setJob);
+}
+
+const setJob = (person) => {
+  person.job = "I'm not NEET!";
+  console.log(person);
+};
+
+createPerson((person, callback) => callback(person, setName));
